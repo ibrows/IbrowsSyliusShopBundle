c3ce832204410e1d65270cb6f40aedecedf7b734
Mike Meier <mike.meier@ibrows.ch>
2013-05-08 14:33:02 +0200

    make fully usable entity - rename to user

--- Entity/User.php
+++ Entity/User.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
  */
-class BaseUser extends FOSUser
+class User extends FOSUser
 {
     /**
      * @var int
@@ -21,7 +21,7 @@ class BaseUser extends FOSUser
 
     /**
      * @param string $email
-     * @return BaseUser
+     * @return User
      */
     public function setEmail($email)
     {
