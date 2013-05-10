8e911457225f32bb17371f66c7c05c0f23ed019a
Mike Meier <mike.meier@ibrows.ch>
2013-05-10 14:41:01 +0200

    own hinclude implementation for attributes handling

--- Twig/TwigExtension.php
+++ Twig/TwigExtension.php
@@ -12,11 +12,28 @@ class TwigExtension extends \Twig_Extension
     protected $currentCartManager;
 
     /**
-     * @param CurrentCartManager $currentCartManager
+     * @var string
      */
-    public function __construct(CurrentCartManager $currentCartManager)
-    {
+    protected $defaultHincludeTemplate;
+
+    /**
+     * @var string
+     */
+    protected $charset;
+
+    /**
+     * @param CurrentCartManager $currentCartManager
+     * @param string $defaultHincludeTemplate
+     * @param string $charset
+     */
+    public function __construct(
+        CurrentCartManager $currentCartManager,
+        $defaultHincludeTemplate,
+        $charset
+    ){
         $this->currentCartManager = $currentCartManager;
+        $this->defaultHincludeTemplate = $defaultHincludeTemplate;
+        $this->charset = $charset;
     }
 
     /**
@@ -25,9 +42,14 @@ class TwigExtension extends \Twig_Extension
     public function getFunctions(){
         return array(
             'getCurrentCartManager' => new \Twig_Function_Method($this, 'getCurrentCartManager'),
-            'getCurrentCart' => new \Twig_Function_Method($this, 'getCurrentCart')
+            'getCurrentCart' => new \Twig_Function_Method($this, 'getCurrentCart'),
+            'ibr_render_hinclude' => new \Twig_Function_Method($this, 'renderHinclude', array(
+                'needs_environment' => true,
+                'is_safe' => array('html')
+            )),
         );
     }
+
     /**
      * @return CurrentCartManager
      */
@@ -42,6 +64,43 @@ class TwigExtension extends \Twig_Extension
     public function getCurrentCart()
     {
        return $this->currentCartManager->getCart();
+    }
+
+    /**
+     * @param \Twig_Environment $environment
+     * @param string $uri
+     * @param array $options
+     * @return Response
+     */
+    public function renderHinclude(\Twig_Environment $environment, $uri, array $options = array())
+    {
+        // We need to replace ampersands in the URI with the encoded form in order to return valid html/xml content.
+        $uri = str_replace('&', '&amp;', $uri);
+
+        $template = isset($options['default']) ? $options['default'] : $this->defaultHincludeTemplate;
+        if ($template) {
+            $content = $environment->render($template);
+        } else {
+            $content = $template;
+        }
+
+        $attributes = isset($options['attributes']) && is_array($options['attributes']) ? $options['attributes'] : array();
+        if (isset($options['id']) && $options['id']) {
+            $attributes['id'] = $options['id'];
+        }
+
+        $renderedAttributes = '';
+        if (count($attributes) > 0) {
+            foreach($attributes as $attribute => $value) {
+                $renderedAttributes .= sprintf(
+                    ' %s="%s"',
+                    htmlspecialchars($attribute, ENT_QUOTES | ENT_SUBSTITUTE, $this->charset, false),
+                    htmlspecialchars($value, ENT_QUOTES | ENT_SUBSTITUTE, $this->charset, false)
+                );
+            }
+        }
+
+        return sprintf('<hx:include src="%s"%s>%s</hx:include>', $uri, $renderedAttributes, $content);
     }
 
     /**
