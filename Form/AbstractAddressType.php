10ba396cc49b9f8ca384dfcc56eb6a5a0f94290a
Mike Meier <mike.meier@ibrows.ch>
2013-05-22 12:35:59 +0200

    allow to set country choices and preferred choices

--- Form/AbstractAddressType.php
+++ Form/AbstractAddressType.php
@@ -8,11 +8,35 @@ use Symfony\Component\Form\FormBuilderInterface;
 abstract class AbstractAddressType extends AbstractType
 {
     /**
+     * @var array
+     */
+    protected $countryChoices = array();
+
+    /**
+     * @var array
+     */
+    protected $preferredCountryChoices = array();
+
+    public function __construct(array $countryChoices = array(), array $preferredCountryChoices = array())
+    {
+        $this->countryChoices = $countryChoices;
+        $this->preferredCountryChoices = $preferredCountryChoices;
+    }
+
+    /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $countryOptions = array();
+        if($this->countryChoices){
+            $countryOptions['choices'] = $this->countryChoices;
+        }
+        if($this->preferredCountryChoices){
+            $countryOptions['preferred_choices'] = $this->preferredCountryChoices;
+        }
+
         $builder
             ->add('firstname')
             ->add('lastname')
@@ -20,7 +44,7 @@ abstract class AbstractAddressType extends AbstractType
             ->add('street')
             ->add('zip')
             ->add('city')
-            ->add('country')
+            ->add('country', 'country', $countryOptions)
             ->add('phone')
             ->add('email', 'email')
         ;
@@ -32,5 +56,42 @@ abstract class AbstractAddressType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         parent::setDefaultOptions($resolver);
+    }
+
+    /**
+     * @return array
+     */
+    public function getPreferredCountryChoices()
+    {
+        return $this->preferredCountryChoices;
+    }
+
+    /**
+     * @param array $preferredCountryChoices
+     * @return AbstractAddressType
+     */
+    public function setPreferredCountryChoices($preferredCountryChoices)
+    {
+        $this->preferredCountryChoices = $preferredCountryChoices;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCountryChoices()
+    {
+        return $this->countryChoices;
+    }
+
+    /**
+     * @param array $countryChoices
+     * @return AbstractAddressType
+     */
+    public function setCountryChoices($countryChoices)
+    {
+        $this->countryChoices = $countryChoices;
+        return $this;
     }
 }
