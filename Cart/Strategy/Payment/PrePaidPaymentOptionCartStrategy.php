a8a7d22217757b5af73e65c58eaee312f3aa7aad
Mike Meier <mike.meier@ibrows.ch>
2013-05-24 13:45:00 +0200

    add PrePaidPaymentOption

---
+++ Cart/Strategy/Payment/PrePaidPaymentOptionCartStrategy.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Ibrows\SyliusShopBundle\Cart\Strategy\Payment;
+
+use Ibrows\SyliusShopBundle\Cart\CartManager;
+use Ibrows\SyliusShopBundle\Cart\Strategy\Payment\Response\ErrorRedirectResponse;
+use Ibrows\SyliusShopBundle\Cart\Strategy\Payment\Response\PaymentFinishedResponse;
+use Ibrows\SyliusShopBundle\Cart\Strategy\Payment\Response\SelfRedirectResponse;
+use Ibrows\SyliusShopBundle\Model\Cart\AdditionalCartItemInterface;
+use Ibrows\SyliusShopBundle\Model\Cart\CartInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class PrePaidPaymentOptionCartStrategy extends AbstractPaymentOptionCartStrategy
+{
+    /**
+     * @param CartInterface $cart
+     * @param CartManager $cartManager
+     * @return bool
+     */
+    public function isPossible(CartInterface $cart, CartManager $cartManager)
+    {
+        return true;
+    }
+
+    /**
+     * @param CartInterface $cart
+     * @param CartManager $cartManager
+     * @return AdditionalCartItemInterface[]
+     */
+    public function compute(CartInterface $cart, CartManager $cartManager)
+    {
+        return array();
+    }
+
+    /**
+     * @param Context $context
+     * @param CartInterface $cart
+     * @param CartManager $cartManager
+     * @return RedirectResponse|PaymentFinishedResponse|ErrorRedirectResponse|SelfRedirectResponse
+     */
+    public function pay(Context $context, CartInterface $cart, CartManager $cartManager)
+    {
+        return new PaymentFinishedResponse();
+    }
+}
