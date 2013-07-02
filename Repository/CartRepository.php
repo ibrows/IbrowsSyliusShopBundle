94076f29420c68c91cd9e0e99b98bac4925e0f43
Daniel Rempfler <daniel.rempfler@ibrows.ch>
2013-07-02 08:53:55 +0200

    Bugfix for intval-Arry-Bug

--- Repository/CartRepository.php
+++ Repository/CartRepository.php
@@ -2,10 +2,23 @@
 
 namespace Ibrows\SyliusShopBundle\Repository;
 
+use Doctrine\DBAL\LockMode;
 use Sylius\Bundle\CartBundle\Entity\CartRepository as BaseCartRepository;
 
 use Doctrine\ORM\EntityRepository;
 
 class CartRepository extends BaseCartRepository
 {
+    /**
+     * Bugfix for intval-Arry-Bug
+     *
+     * @param mixed $id
+     * @param $lockMode
+     * @param null $lockVersion
+     * @return mixed|null|object
+     */
+    public function find($id, $lockMode = LockMode::NONE, $lockVersion = null)
+    {
+        return $this->_em->find($this->_entityName, $id, $lockMode, $lockVersion);
+    }
 }
