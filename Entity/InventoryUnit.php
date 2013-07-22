d658178c5141f3db4cce952c69a21acaf63fa99a
Mike Meier <mike.meier@ibrows.ch>
2013-07-22 15:29:01 +0200

    correct use statement

--- Entity/InventoryUnit.php
+++ Entity/InventoryUnit.php
@@ -3,7 +3,7 @@
 namespace Ibrows\SyliusShopBundle\Entity;
 
 use Ibrows\SyliusShopBundle\Model\Cart\CartInterface;
-use spec\Sylius\Bundle\InventoryBundle\Model\InventoryUnit as BaseInventoryUnit;
+use Sylius\Bundle\InventoryBundle\Model\InventoryUnit as BaseInventoryUnit;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
