4430a5e8b6d9123956c0230087f2de3e2d18d5dc
Mike Meier <mike.meier@ibrows.ch>
2013-10-31 19:41:28 +0100

    format

--- Cart/Strategy/Currency/AbstractCurrencyCartStrategy.php
+++ Cart/Strategy/Currency/AbstractCurrencyCartStrategy.php
@@ -2,8 +2,6 @@
 
 namespace Ibrows\SyliusShopBundle\Cart\Strategy\Currency;
 
-use Ibrows\SyliusShopBundle\Cart\CartManager;
-use Ibrows\SyliusShopBundle\Model\Cart\CartInterface;
 use Ibrows\SyliusShopBundle\Model\Cart\Strategy\CartCurrencyStrategyInterface;
 
 abstract class AbstractCurrencyCartStrategy implements CartCurrencyStrategyInterface
