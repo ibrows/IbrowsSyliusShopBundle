8d41cf97cfeaff2e9926a23ef67adbb645bee65a
Mike Meier <mike.meier@ibrows.ch>
2013-10-31 19:41:35 +0100

    add docs

--- Model/Cart/Strategy/CartCurrencyStrategyInterface.php
+++ Model/Cart/Strategy/CartCurrencyStrategyInterface.php
@@ -7,9 +7,35 @@ use Ibrows\SyliusShopBundle\Model\Cart\CartInterface;
 
 interface CartCurrencyStrategyInterface
 {
+    /**
+     * @param CartInterface $cart
+     * @param CartManager $cartManager
+     * @return CartCurrencyStrategyInterface
+     */
     public function acceptAsDefaultCurrency(CartInterface $cart, CartManager $cartManager);
+
+    /**
+     * @param CartInterface $cart
+     * @param CartManager $cartManager
+     * @return string
+     */
     public function getDefaultCurrency(CartInterface $cart, CartManager $cartManager);
 
+    /**
+     * @param string $fromCurrency
+     * @param string $toCurrency
+     * @param CartInterface $cart
+     * @param CartManager $cartManager
+     * @return bool
+     */
     public function acceptCurrencyChange($fromCurrency, $toCurrency, CartInterface $cart, CartManager $cartManager);
+
+    /**
+     * @param string $fromCurrency
+     * @param string $toCurrency
+     * @param CartInterface $cart
+     * @param CartManager $cartManager
+     * @return void
+     */
     public function changeCurrency($fromCurrency, $toCurrency, CartInterface $cart, CartManager $cartManager);
 }
