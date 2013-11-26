982a7db69faf4421cee119e6d40223c707edd83f
Mike Meier <mike.meier@ibrows.ch>
2013-11-26 10:09:50 +0100

    test setup

---
+++ Tests/Cart/Strategy/Voucher/VoucherCartStrategyTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ibrows\SyliusShopBundle\Tests\Cart\Strategy\Voucher;
+
+class VoucherCartStrategy extends \PHPUnit_Framework_TestCase
+{
+    public function testComputeAdditionalItems()
+    {
+
+    }
+}
