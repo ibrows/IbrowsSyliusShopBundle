a5d2f594f13cc1a814027f4c4f8d831d7ebfb99d
Mike Meier <mike.meier@ibrows.ch>
2013-11-27 17:24:38 +0100

    do not recompute on payment information steps

--- Controller/AbstractWizardController.php
+++ Controller/AbstractWizardController.php
@@ -148,7 +148,7 @@ abstract class AbstractWizardController extends AbstractController
 
         $cart->addPayment($payment);
 
-        $this->persistCart($cartManager, false);
+        $this->persistCart($cartManager);
     }
 
     /**
@@ -164,7 +164,7 @@ abstract class AbstractWizardController extends AbstractController
         $cart->setConfirmed();
         $cart->setPayed();
 
-        $this->persistCart($cartManager, false);
+        $this->persistCart($cartManager);
 
         return $this->redirect($this->getWizard()->getNextStepUrl());
     }
@@ -225,7 +225,7 @@ abstract class AbstractWizardController extends AbstractController
         $cartManager = $this->getCurrentCartManager();
 
         $cart->setConfirmed();
-        $this->persistCart($cartManager, false);
+        $this->persistCart($cartManager);
 
         return $this->redirect($this->getWizard()->getNextStepUrl());
     }
