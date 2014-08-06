6710838c78f8aa645a6d821a064d3cdf5d6beb77
Dominik Zogg <dominik.zogg@gmail.com>
2014-08-06 10:22:36 +0200

    fix auth form validation

--- Form/AuthType.php
+++ Form/AuthType.php
@@ -3,6 +3,8 @@
 namespace Ibrows\SyliusShopBundle\Form;
 
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class AuthType extends AbstractType
 {
@@ -12,7 +14,12 @@ class AuthType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('email', 'email');
+        $builder->add('email', 'email', array(
+            'constraints' => array(
+                new NotBlank(array('groups' => array('sylius_wizard_auth'))),
+                new Email(array('groups' => array('sylius_wizard_auth'))),
+            )
+        ));
     }
 
     /**
